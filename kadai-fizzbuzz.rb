f5614bb808c557a21c6625006b039660f6190b9a
kmbnkyu <kmogksy353688@gmail.com>
2022-05-10 06:54:22 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(num)
    if num % 15 == 0 
-     puts 'FizzBuzz'
+     return 'FizzBuzz'
    elsif num % 5 == 0
-     puts 'Buzz'
+     return 'Buzz'
    elsif num % 3 == 0 
-     puts 'Fizz'
+     return 'Fizz'
    else 
-     puts num
+     num
    end
 end
 num_max = 100
 (1..num_max).each do |num|
-  fizzbuzz(num)
+  puts fizzbuzz(num)
 end
 
 
